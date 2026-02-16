e172605bd5106fee0da81e703f1af1080b8ab782
xllyllz <82016137+210213216219@users.noreply.github.com>
2026-02-16 18:19:56 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 {
   function onYouTubeIframeAPIReady() {
-    let player = new YT.Player("player", {
+    player = new YT.Player("player", {
       videoId: "Rk54JNn7Qw4",
       playerVars: { mute: 1 },
       events: {
         onStateChange: e => {
           let data = e.data;
+          console.log(data);
           if (data == 1) {
             vocal.play();
             track.play();
@@ -19,6 +20,10 @@
   }
   let vocal = new Audio("vocal.flac");
   let track = new Audio("track.flac");
-  player.nextElementSibling.oninput = e => vocal.volume = +e.target.value * .01;
-  player.nextElementSibling.nextElementSibling.oninput = e => track.volume = +e.target.value * .01;
+
+  vocal.volume = +(v0.textContent = localStorage.getItem("0") ?? (localStorage.setItem("0", 50), 50)) * .01;
+  track.volume = +(v1.textContent = localStorage.getItem("1") ?? (localStorage.setItem("1", 50), 50)) * .01;
+
+  s0.oninput = e => vocal.volume = +(v0.textContent = e.target.value) * .01;
+  s1.oninput = e => track.volume = +(v0.textContent = e.target.value) * .01;
 }

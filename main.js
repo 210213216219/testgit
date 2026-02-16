6f1422f345d8343cf1ae4f1f15c57aa8f2b1f285
xllyllz <82016137+210213216219@users.noreply.github.com>
2026-02-16 11:43:52 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+{
+  function onYouTubeIframeAPIReady() {
+    let player = new YT.Player("player", {
+      videoId: "Rk54JNn7Qw4",
+      playerVars: { mute: 1 },
+      events: {
+        onStateChange: e => {
+          let data = e.data;
+          if (data == 1) {
+            vocal.play();
+            track.play();
+          } else if (data == 2) {
+            vocal.pause();
+            track.pause();
+          }
+        }
+      }
+    });
+  }
+  let vocal = new Audio("vocal.flac");
+  let track = new Audio("track.flac");
+  player.nextElementSibling.oninput = e => vocal.volume = +e.target.value * .01;
+  player.nextElementSibling.nextElementSibling.oninput = e => track = +e.target.value * .01;
+}
